b0ff6d11e2396943e52097ae7ffcbe055433821c
Hetul Suthar <83918050+hetul0012@users.noreply.github.com>
2026-03-01 22:48:04 -0500

    Change API base URL to production server

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://healthease-multi-specialty-hospital.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
