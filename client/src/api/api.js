0774afbeda71cd6666229cbab0597d093591735e
Hetul Suthar <83918050+hetul0012@users.noreply.github.com>
2026-03-02 23:10:54 -0500

    Update API base URL to use environment variable

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthease-multi-specialty-hospital.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
