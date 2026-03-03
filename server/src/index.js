2f6071b95b2ac2560d79606919f491206cf88000
Hetul Suthar <83918050+hetul0012@users.noreply.github.com>
2026-03-02 22:05:44 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -26,10 +26,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://your-vercel-app.vercel.app"
-    ],
+    origin: true,
     credentials: true,
   })
 );
